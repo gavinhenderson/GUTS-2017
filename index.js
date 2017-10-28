76f87b49d95750ca3ac2ff515bf02da6e7e540e4
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2017-10-28 01:29:08 +0100

    Some socket stuff done

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
-var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 app.use(express.static('public'))
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(80);
+io.on('connection', function(socket){
+  io.emit('handshake', 'room1');
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(80, function(){
+  console.log('listening on *:3000');
+});
