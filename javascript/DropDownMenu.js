de502bec7e657850d20411fcd8f4f78315d90bc4
Jay Bennett <bnnttjy@googlemaill.com>
2017-10-28 11:41:51 +0100

    Made a drop down menu on a click

--- javascript/DropDownMenu.js
+++ javascript/DropDownMenu.js
@@ -1,56 +1,52 @@
-  var sel;  
-  var isPressed; 
-  var xCoordinate;
-var yCoordinate; 
-function setup() {
-  createCanvas(700, 700);
-  isPressed = false; 
-}
-
-function mySelectEvent() {
-  var item = sel.value();
-  background(200);
-  text("it's a "+item+"!", 50, 50);
-}
-
-function mouseClicked()
+function DropDownMenu(list)
 {
-	console.log(isPressed);
+	this.sel;  
+	this.isPressed = false; 
+	this.xCoordinate;
+	this.Coordinate;
 	
-
-	if(isPressed == false)	
-	{
-		
-		isPressed = true; 
-		textAlign(CENTER);
-		background(200);
-		sel = createSelect();
-		xCoordinate = mouseX;
-		yCoordinate = mouseY; 
-		sel.position(xCoordinate, yCoordinate);
-		sel.size(100,100)
-		sel.option('this');
-		sel.option('shit');
-		sel.option('works');
-
-		sel.changed(mySelectEvent);
-	}
-	else if(collidePointRect(mouseX,mouseY,xCoordinate,yCoordinate, 100,100) && isPressed)
-	{
-		sel.changed(mySelectEvent);
-	}
-	else
-	{
-		isPressed = false; 
-		sel.remove(); 
-	}
-}
-
-
-
-function DropDownMenu()
-{
-	this.mouseClicked = function(){	
-		
+	this.mySelectEvent = function()
+	{ 
+		var elem = document.getElementById("dropDownList");
+		console.log(elem);
+		var item = elem.value; 
+  		background(200);
+  		text("it's a "+ item+"!", 50, 50);
 	};
+
+	this.mouseMenu = function(){	
+	
+		if(this.isPressed == false)	
+		{
+			
+			this.isPressed = true; 
+			textAlign(CENTER);
+			background(200);
+			this.sel = createSelect();
+			this.sel.id("dropDownList"); 
+			this.xCoordinate = mouseX;
+			this.yCoordinate = mouseY; 
+			this.sel.position(this.xCoordinate, this.yCoordinate);
+			this.sel.size(100,100)
+			for(var i=0; i<list.length; i++)
+			{
+				this.sel.option(list[i]); 
+			}
+
+			var elem = document.getElementById("dropDownList").value="";
+
+			this.sel.changed(this.mySelectEvent);
+		}
+		else if(collidePointRect(mouseX,mouseY,this.xCoordinate,this.yCoordinate, 100,100) && this.isPressed)
+		{
+			var elem = document.getElementById("dropDownList").value="";
+			this.sel.changed(this.mySelectEvent);
+		}
+		else
+		{
+			this.isPressed = false; 
+			this.sel.remove(); 
+		}
+	};
+
 }
