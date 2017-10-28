0e0ecbc0c0e5b5a6936698539ef7dd9147b378a4
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2017-10-28 16:22:57 +0100

    removed debug prints

--- javascript/socket.js
+++ javascript/socket.js
@@ -1,17 +1,17 @@
 function setCoordinates(x,y){
-	console.log("setting coords");
+	//console.log("setting coords");
 	myRoom.emit('setCoordinates',{'x':x,'y':y});
 }
 
 function getCoordinates(){
-	console.log("getting coords")
+	//console.log("getting coords")
 	myRoom.emit('getCoordinates')
 }
 
 myRoom.on('updateCoords',function(coords){
   player.x = coords.x;
   player.y = coords.y;
-  console.log(coords);
+  //console.log(coords);
 });
 
 function getPlayerNo(){
